6d78808179d33c4705d5cb7a98b574e35077b5c4
NycolasDarosci <nycoollaas@gmail.com>
2020-11-17 01:43:06 -0300

    fim da aula #3

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -55,9 +55,43 @@ function addPhotoField(){
 
 }
 
-//function deleteField(event){
-//    cont span = event.currentTarget
+function deleteField(event){
+    const span = event.currentTarget
 
-//}
+    const fieldsContainer = document.querySelectorAll('.new-upload')
 
+    if(fieldsContainer.length < 2){
+        //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove();
+
+
+}
+
+
+//selecionar sim e não
+function toggleSelect (event){
+    //retirar a class.active (dos botões)
+    document.querySelectorAll('.button-select button')
+    .forEach(function(button){
+        button.classList.remove('active')
+    })
+
+    //colocar a class.active nesse botao clicado 
+
+    const button = event.currentTarget
+    button.classList.add('active')
+
+
+    //atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    input.value = button.dataset.value
+    
+
+}
 
