30c181b9cce2cbfa34bb30e524970abfc52434fe
NycolasDarosci <nycoollaas@gmail.com>
2020-11-23 10:05:34 -0300

    minuto 40 #4

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('efefefefef')
+//importar dependência
+const express = require('express');
+
+//iniciando o express
+const server = express()
+
+//criar uma rota
+server.get('/', (request, response) => {
+    return response.send('Oi direto do')
+})
+
+//ligar o servidor
+server.listen(5500)
