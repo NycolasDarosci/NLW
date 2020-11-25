606375b7d6f50ce44a0ddd933acf16c25f69c343
NycolasDarosci <nycoollaas@gmail.com>
2020-11-25 09:19:33 -0300

    problemas 1:34 #4

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+
 //importar dependência
 const express = require('express');
 const path = require('path');
+const pages = require('./pages.js');
+
+
 
 //iniciando o express
 const server = express()
+server
 
-//criar uma rota
-server.get('/', (request, response) => {
+    //utilizando os arquivos estaticos
+    .use(express.static('public'))
+
+    //configurar template engine
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'hbs')
+
+    //rotas de aplicação
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
     
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
 
 //ligar o servidor
 server.listen(5500)
